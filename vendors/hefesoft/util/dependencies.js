a95d331e445753779f0bf24c0d780604f90660af
Jose douglas ramirez <chibchombiano26@hotmail.com>
2015-09-18 07:23:26 +0000

    17 de julio

---
+++ vendors/hefesoft/util/dependencies.js
@@ -0,0 +1,52 @@
+
+
+function dependenciasNews(){
+    
+    var dp = [
+            /* Odontologos.com.co */
+            'js/hefesoft/Noticias/Controllers/odontologos.com.co.js',
+            'js/hefesoft/Noticias/Directivas/directive/odontologos.com.co.js',
+            
+            /* Fb page */
+            'js/hefesoft/Noticias/Controllers/asociacionOdontologos.js',
+            'js/hefesoft/Noticias/Directivas/directive/asociacionOdontologos.js',
+            'vendors/hefesoft/fb/services/asociasionOdontologos.js',
+            
+            /* Import.io */
+            'vendors/hefesoft/import.io/services/read.js',
+            'vendors/hefesoft/Directivas/nhRef/nhref.js',
+            
+            /* lodash */
+            'vendors/bower_components/lodash/lodash.min.js'
+        ]
+    return dp;
+    
+}
+
+
+function dependenciasGalleryAppLevel(){
+    var dp = [
+            'vendors/bower_components/autosize/dist/autosize.min.js',
+            'vendors/bower_components/lightgallery/light-gallery/css/lightGallery.css'
+        ]
+        
+    return dp;
+}
+
+function dependenciasGalleryAppLevel(){
+    var dp = [
+            'vendors/bower_components/autosize/dist/autosize.min.js',
+            'vendors/bower_components/lightgallery/light-gallery/css/lightGallery.css'
+        ]
+        
+    return dp;
+}
+
+function dependenciasGallery(){
+    var dp = [
+                'vendors/bower_components/mediaelement/build/mediaelement-and-player.js',
+                'vendors/bower_components/lightgallery/light-gallery/js/lightGallery.min.js'
+            ]
+        
+    return dp;
+}
